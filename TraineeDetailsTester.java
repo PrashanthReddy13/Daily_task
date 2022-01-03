16a8bcb03201dfa31671174929d7264c6358dd8b
Prashanth S <92350320+prashanthrddy13@users.noreply.github.com>
2022-01-03 22:18:43 +0530

    Add files via upload

---
+++ TraineeDetailsTester.java
@@ -0,0 +1,7 @@
+class TraineeDetailsTester{
+	public static void main(String []ar){
+		TraineeDetails.updateName("null","Prashanth");
+		System.out.println("========================================");
+		TraineeDetails.printAll();
+	}
+} 
