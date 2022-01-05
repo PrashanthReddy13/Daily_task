acd3624009997cdc369754147693cfd91a6782b3
Prashanth S <92350320+prashanthrddy13@users.noreply.github.com>
2022-01-05 23:07:37 +0530

    Add files via upload

---
+++ MinicoopercarTester.java
@@ -0,0 +1,37 @@
+class DustercarTester{
+	
+	public static void main(String []args){
+		Dustercar.speed();
+		int time=13;
+		Dustercar.speed(time);
+		int details= 14; String acceleter="to 150 speed";
+		Dustercar.speed(details, acceleter);
+		String piston= "800 rpm"; int cylinder= 40;
+		Dustercar.speed(piston, cylinder);
+		System.out.println("music======feature");
+		Dustercar.music();
+		int topmusic=13;
+		Dustercar.music(topmusic);
+		int topmusic2= 14; String topmusic3="to 150 speed";
+		Dustercar.music(topmusic2, topmusic3);
+		String topmusic4= "800 rpm"; int topmusic5= 40;
+		Dustercarr.music(topmusic4, topmusic5);
+		System.out.println("resting======feature");
+		Dustercar.resting();
+		int jkll=13;
+		Dustercar.resting(jkll);
+		int qwer= 14; String tyui="to 150 speed";
+		Dustercar.resting(qwer, tyui);
+		String opas= "800 rpm"; int dfgh= 40;
+		Dustercar.resting(opas, dfgh);
+		System.out.println("travel======feature");
+		Dustercar.travel();
+		int location=13;
+		Dustercar.travel(location);
+		int location2= 14; String location3="to 150 speed";
+		Dustercar.travel(location2, location3);
+		String location4= "800 rpm"; int location5= 40;
+		Dustercar.travel(location4, location5);
+	}
+
+}
