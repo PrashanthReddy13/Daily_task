e1816a69e29db548e33d477755284383b4c1a1c5
Prashanth S <92350320+prashanthrddy13@users.noreply.github.com>
2022-01-05 23:06:56 +0530

    Add files via upload

---
+++ MinicooperCar.java
@@ -0,0 +1,51 @@
+class MinicooperCar{
+	static void speed(){
+		System.out.println("6000rpm");
+	}
+	
+	static void speed(int time){
+		System.out.println("5000rpm");
+		
+	} 
+	static void speed(int details, String acceleter){
+		System.out.println("4500rpm");
+	}
+	static void speed(String piston, int cylinder){
+		System.out.println("8000rpm");
+	}
+	static void music(){
+		System.out.println("veera--kannadiga");
+	}
+	static void music(int topmusic){
+		System.out.println("rcb--tanthem");
+	} 
+	static void music(int topmusic2, String topmusic3){
+		System.out.println("lakalaka--lamborgini");
+	}
+	static void music(String topmusic4, int topmusic5){
+		System.out.println("OGA");
+	}
+	static void resting(){
+		System.out.println("sleeping");
+	}
+	static void resting(int jkll){
+		System.out.println("seeingmovie");
+	} 
+	static void resting(int qwer, String tyui){
+		System.out.println("camping");
+	}
+	static void resting(String opas, int dfgh){
+		System.out.println("chilling");
+	}
+	static void travel(){
+		System.out.println("mangaluru");
+	}
+	static void travel(int location){
+		System.out.println("mysore");
+	} 
+	static void travel(int location2, String location3){
+		System.out.println("hassan");
+	}
+	static void travel(String location4, int location5){
+		System.out.println("madikeri");
+}
