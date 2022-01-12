3293d908ce3c5481104d95b5c50c22616ab493c2
prashanth <prashanth.xworkz@gmail.com>
2022-01-12 08:54:37 +0530

    class

---
+++ Details.java
@@ -0,0 +1,16 @@
+class Details{ // class object
+    String name;
+	int id;
+	long number;
+	String dOB;
+	String email;
+
+    Details(String nameOf,int idOf,long numberOf,String dOBOf,String emailId){
+	 id=idOf;
+	 name=nameOf;
+	 number=numberOf;
+	 dOB=dOBOf;
+	 email=emailId;
+	}
+}
+ 
---
+++ forloopdemo.java
@@ -0,0 +1,6 @@
+Class Forloopdemo{
+publicstaticvoidmain(String[]args);
+for(int i=2;i<=20;i++){
+System.out.println("i");
+}
+}
