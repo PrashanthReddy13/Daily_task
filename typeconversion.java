414c327ecd29277d997c1ccf63a4aee790650f1e
Prashanth S <92350320+PrashanthReddy13@users.noreply.github.com>
2022-01-20 22:26:15 +0530

    type_conersion

---
+++ typeconversion.java
@@ -0,0 +1,11 @@
+class TypeConversion{
+	public static void main(String []arg){
+		//widning
+		byte number=9;
+		int num=number;
+		System.out.println(num);
+		int age=35;
+		byte nage=(byte)35;
+		System.out.println(nage);
+	}
+}
